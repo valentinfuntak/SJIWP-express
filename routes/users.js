205734638f4ee98914808869eab8a17de76844cd
ValentinFuntak <valentinfuntakk@gmail.com>
2023-12-12 09:25:53 +0100

    Dorađene poruke za greške i korisnicke i serverske validacije

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var Joi = require("joi");
 
-router.get('/signin', function(req, res, next) {
-  res.render('users/signin', { title: 'Prijava korisnika' });
+router.get('/signin', function (req, res, next) {
+  res.render('users/signin', { status: { Display_form :true , error: false } });
+});
+
+const schema_signin = Joi.object({
+  email: Joi.string().email().max(50).required(),
+  password: Joi.string().max(50).required()
+});
+
+router.post('/signin', function (req, res, next) {
+  const data = req.body;
+
+  const validation = schema_signin.validate(data);
+  if (validation.error) {
+    //dogodila se greška u podacima
+
+    res.render('users/signin', { status: { Sumbit_form :true, error: true } });
+  }
+  else{
+    //TODO: Prijava korisnika
+    res.render('users/signin', { status: {  Sumbit_form: true, error: false } });
+  }
 });
 
 module.exports = router;
